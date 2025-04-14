bb5f588bf53e88e3a1421ebebd0e7b60ab9d6402
ya924 <angel09240811@gmail.com>
2025-04-14 20:00:12 +0800

    Create 6.php

---
+++ 6.php
@@ -0,0 +1,17 @@
+<?php
+   #mysqli_connect() 建立資料庫連結
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+   #mysqli_query() 從資料庫查詢資料
+   $result=mysqli_query($conn, "select * from user");
+   #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
+   $login=FALSE;
+   while ($row=mysqli_fetch_array($result)) {
+     if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
+       $login=TRUE;
+     }
+   } 
+   if ($login==TRUE)
+     echo "登入成功";
+  else
+     echo "帳號/密碼 錯誤";
+?>
