1f06a09342d29b5895ac930a21b915c827a4aca7
ya924 <angel09240811@gmail.com>
2025-04-14 19:52:18 +0800

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>html+php</title> <!--標題-->
+    </head>      
+    <body>
+        <?php echo "Hello world!"; ?> <!--Hello world!-->
+        <h1>html</h1> <!--標題-->
+        <?php echo "Hello world!"; ?>
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?>
+    </body>
+</html>
